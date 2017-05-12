cc9a3f91cf3f762be8f12978fa9ae50681d12f98
Game110AM <Game110AM@605-110-STU03>
2017-05-12 10:43:12 -0600

    invisible snaks

--- Ekans.cpp/Ekans.cpp/Snek.cpp
+++ Ekans.cpp/Ekans.cpp/Snek.cpp
@@ -10,10 +10,8 @@
 using namespace std;
 
 
-
-
-
 int main() {
+
 	int SCREEN_W = 800;
 	int SCREEN_H = 800;
 	int head_x = 1;
@@ -22,10 +20,20 @@ int main() {
 	int snak_y = 2;
 	int sneksize = 40;
 	bool key[4]{ false, false, false, false };
-	int snake_length = 2;
+	int snake_length = 0; //tail length
 	bool Eatgoal = false;
 	bool doexit = false;
 	bool redraw = true;
+	bool freshsnak = false;
+
+	al_init();
+	al_install_keyboard();
+	al_install_audio();
+	al_init_acodec_addon();
+	al_init_primitives_addon();
+	al_init_image_addon();
+	al_init_font_addon();
+	al_init_ttf_addon();
 	ALLEGRO_DISPLAY*display = al_create_display(SCREEN_W, SCREEN_H);
 	ALLEGRO_BITMAP*snake = NULL;
 	ALLEGRO_FONT*font = NULL;
@@ -39,32 +47,20 @@ int main() {
 	vector<int>::const_iterator iter;
 
 
-
-
-
-
-
-	al_init();
-	al_install_keyboard();
-	al_install_audio();
-	al_init_acodec_addon();
-	al_init_primitives_addon();
-	al_init_image_addon();
-	al_init_font_addon();
-	al_init_ttf_addon();
 	al_reserve_samples(2);
 	//	music = al_load_sample("music.wav");
 	//  al_play_sample(music, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
 	font = al_create_builtin_font();
-	timer = al_create_timer(0.02);
+	timer = al_create_timer(0.2);
 	display = al_create_display(SCREEN_W, SCREEN_H);
-
+	
 	int grid[20][20];
-	for (int i = 0; i > 20; i++)
-		for (int j = 0; j > 20; j++)
+	for (int i = 0; i < 20; i++)
+		for (int j = 0; j < 20; j++)
 			grid[i][j] = 0;
 
-	grid[snak_x][snak_y] = 2;
+	grid[snak_x][snak_y] =2;
+
 	srand(time(NULL));
 	al_set_target_bitmap(al_get_backbuffer(display));
 	event_queue = al_create_event_queue();
@@ -75,57 +71,117 @@ int main() {
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 	al_flip_display();
 	al_start_timer(timer);
+	//cout << "Flag1" << endl;
+	while (doexit != true) {
 
-	while (doexit != false) {
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
-		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 20; j++)
-				cout << grid[i][j] << endl;
-		}
-		//		if (ev.type == ALLEGRO_EVENT_TIMER) {
-		//			for (int i = 0; i < 20; i++)
-		//				for (int j = 0; j < 20; j++) {
-		//					if (grid [i][j] == 1)
 
-		//				}
+
+		//for (int i = 0; i < 20; i++) {
+		//	for (int j = 0; j < 20; j++)
+		//		cout << grid[i][j] ;
+		//	cout << endl;
+		//}
+
 
 		if (ev.type == ALLEGRO_EVENT_TIMER) {
+			
+			//wipe map
 			for (int i = 0; i < 20; i++)
 				for (int j = 0; j < 20; j++) {
-					//					if (grid[i][j] == 1)
+					if (grid[i][j] == 1)
+						grid[i][j] = 0;
+
 
 				}
+			grid[snak_x][snak_y] = 2;
+
+	//redraw snek
 			for (int i = 0; i < snake_length; i = i + 2) {
+				cout << "snek tail is at " << nodes[i] << " , " << nodes[i + 1] << endl;
 				grid[nodes[i]][nodes[i + 1]] = 1;
+
 			}
 
-			if (grid[snak_x] == grid[head_x] && grid[snak_y] == grid[head_y]) {
+			cout << "snak is at " << snak_x<< " , " << snak_y << endl;
+	
+			//check if snek 8 snak
+			if (grid[snak_x] == grid[head_x] && grid[snak_y] == grid[head_y] /*&& freshsnak == true*/) {
 				Eatgoal = true;
+				cout << "om nom" << endl;
+				//freshsnak = false;
 				snake_length += 2;
 			}
 
 
-			if (Eatgoal == true) {
+			//redraw snack
+			if (Eatgoal == true /*&& freshsnak == false*/) {
 				grid[snak_x][snak_y] = 0;
+				cout << "snak despensing!" << endl;
 				snak_x = rand() % 20;
 				snak_y = rand() % 20;
-				grid[snak_x][snak_y] = 2;
 				Eatgoal = false;
+				//freshsnak = true;
 				cout << "Goal @ " << snak_x << ", " << snak_y << endl;
 			}
 
-			if (key[0])
+			//move snek head
+			if (key[0] && head_y > 0)
 				head_y -= 1;
-			else if (key[1])
+			if (key[1] && head_y < 20)
 				head_y += 1;
-			else if (key[2])
+			if (key[2] && head_x > 0)
 				head_x -= 1;
-			else if (key[3])
+			if (key[3] && head_x < 20)
 				head_x += 1;
 
+	
+
+			//draw snek head to map
+			if (head_x >= 0 &&
+				head_y >= 0 &&
+				head_x < 20 &&
+				head_y < 20)
+				grid[head_x][head_y] = 1;
+
+			//push the coordinates into the vector
+			nodes.insert(nodes.begin(), head_y);
+			nodes.insert(nodes.begin(), head_x);
+
+			if (head_x < 0 || head_y < 0)
+				cout << "MATRIX ERROR" << endl;
+
+			if (head_x <= -1 || head_x > 19 || head_y <= -1 || head_y > 19) {
+				cout << "Hit a wall, snek is ded" << endl;
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				al_draw_textf(font, al_map_rgb(255, 100, 100), 300, 300, NULL, "Snek done dedded dude");
+				al_flip_display();
+				al_rest(2);
+				return 0;
+			}
+
+			for (iter = nodes.begin() + 2; iter < nodes.end(); iter += 2) {
+				//cout << "Head is " << head_x << ", " << head_y << endl;
+				//cout << "Node values are " << *iter << ", " << *(iter + 1) << endl;
 
 
+				if ((head_x == *iter &&  head_y == *(iter + 1))) {
+					//cout << "Snek tail killed snek" << endl;
+					al_clear_to_color(al_map_rgb(0, 0, 0));
+					al_draw_textf(font, al_map_rgb(255, 100, 100), 300, 300, NULL, "Snek done dedded dude");
+					al_flip_display();
+					al_rest(2);
+					return 0;
+				}
+			}
+
+			//erase nodes that have "fallen" off the end of the snek
+			nodes.erase(nodes.begin() + snake_length, nodes.end());
+
+			redraw = true;
+
+		}//end timer section
 
 			else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
 
@@ -164,25 +220,24 @@ int main() {
 					key[2] = false;
 					break;
 				}
-
 			}
 			//has something been released on the keyboard?
 			else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
 				switch (ev.keyboard.keycode) {
 				case ALLEGRO_KEY_UP:
-					key[0] = false;
+					//key[0] = false;
 					break;
 
 				case ALLEGRO_KEY_DOWN:
-					key[1] = false;
+					//key[1] = false;
 					break;
 
 				case ALLEGRO_KEY_LEFT:
-					key[2] = false;
+					///key[2] = false;
 					break;
 
 				case ALLEGRO_KEY_RIGHT:
-					key[3] = false;
+					//key[3] = false;
 					break;
 
 					//kill the program if someone presses escape
@@ -192,71 +247,38 @@ int main() {
 				}
 			}
 
-			if (head_x > 0 &&
-				head_y > 0 &&
-				head_x < 20 &&
-				head_y < 20)
-				grid[head_x][head_y] = 1;
-
-			nodes.insert(nodes.begin(), head_y);
-			nodes.insert(nodes.begin(), head_x);
-
-			if (head_x < 0 || head_y < 0)
-				cout << "MATRIX ERROR" << endl;
-
-			if (head_x < 0 || head_x > 20 || head_y < 0 || head_y > 20) {
-				cout << "Hit a wall, snek is ded" << endl;
-				al_clear_to_color(al_map_rgb(0, 0, 0));
-				al_draw_textf(font, al_map_rgb(255, 100, 100), 300, 300, NULL, "Snek done dedded dude");
-				al_flip_display();
-				al_rest(2);
-				return 0;
-			}
-
-			for (iter = nodes.begin() + 2; iter < nodes.end(); iter += 2) {
-				cout << "Head id " << head_x << ", " << head_y << endl;
-				cout << "Node values are " << *iter << ", " << *(iter + 1) << endl;
-
-
-				if ((head_x == *iter &&  head_y == *(iter + 1))) {
-					cout << "Snek tail killed snek" << endl;
-					al_clear_to_color(al_map_rgb(0, 0, 0));
-					al_draw_textf(font, al_map_rgb(255, 100, 100), 300, 300, NULL, "Sned done dedded dude");
-					al_flip_display();
-					al_rest(2);
-					return 0;
-				}
-			}
-			nodes.erase(nodes.begin() + snake_length, nodes.end());
-			redraw = true;
-
-
-
-
-
-
-		}
 		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 			break;
 		}
+
 		if (redraw && al_is_event_queue_empty(event_queue)) {
+			//cout << "Flag3" << endl;
 			redraw = false;
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 			if (Eatgoal == false)
-				grid[snak_x][snak_y] = 2;
+				grid[snak_x][snak_y] = 0;
 			for (int i = 0; i < 20; i++)
 				for (int j = 0; j < 20; j++) {
+					//0 is background
+					if (grid[i][j] == 0)
+						al_draw_filled_rectangle(i*sneksize, j*sneksize, i*sneksize + sneksize, j*sneksize + sneksize, al_map_rgb(200, 100, 150));
+
+					//1 is snek
 					if (grid[i][j] == 1)
 						al_draw_filled_rectangle(i*sneksize, j*sneksize, i*sneksize + sneksize, j*sneksize + sneksize, al_map_rgb(50, 250, 50));
-					if (grid[i][j] == 0)
+
+					//2 is snak
+					if (grid[i][j] == 2) {
 						al_draw_rectangle(i*sneksize, j*sneksize, i*sneksize + sneksize, j*sneksize + sneksize, al_map_rgb(250, 250, 250), 4);
-					if (grid[i][j] == 2)
-						al_draw_filled_rectangle(i*sneksize, j*sneksize, i*sneksize + sneksize, j*sneksize + sneksize, al_map_rgb(100, 100, 150));
-					if (Eatgoal = true)
+						cout << "drawing snak at " << i << " ," << j << endl;
+					}
+
+					if (Eatgoal == true)
 						grid[snak_x][snak_y] = 0;
-					al_flip_display();
+			
 				}
-		}
+			al_flip_display();
+		}//end render
 	}
 	al_destroy_bitmap(snake);
 	al_destroy_timer(timer);
